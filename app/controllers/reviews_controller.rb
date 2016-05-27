6b637035b8df01e126b8f32987e580f29f96a3e6
Jeff Cohen <cohen.jeff@gmail.com>
2016-05-27 18:33:40 -0500

    Mass assignment and make the review form work

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,18 @@
 class ReviewsController < ApplicationController
 
   def create
-    # TODO: create review here
 
-    redirect_to movie_url(params[:movie_id])
+    # Review.new(....)
+    # Review.create(....)
+    # review = Review.find_by(id: 5)
+    # review.update(.....)
+
+    review_params = params.require(:review).permit(:movie_id, :rating, :content)
+    review = Review.new(review_params)
+    review.user_id = session[:user_id]
+    review.save
+
+    redirect_to movie_url(params[:review][:movie_id])
   end
 
 
