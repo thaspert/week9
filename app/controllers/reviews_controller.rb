a15636951f49eda3c5a3f6b6adfb1748c6e1fb5a
Jeff Cohen <cohen.jeff@gmail.com>
2016-05-27 19:35:28 -0500

    Use Rails' ajax technique

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,11 +8,18 @@ class ReviewsController < ApplicationController
     # review.update(.....)
 
     review_params = params.require(:review).permit(:movie_id, :rating, :content)
-    review = Review.new(review_params)
-    review.user_id = session[:user_id]
-    review.save
+    @review = Review.new(review_params)
+    @review.user_id = session[:user_id]
+    @review.save
 
-    redirect_to movie_url(params[:review][:movie_id])
+    respond_to do |format|
+      format.html do
+        redirect_to movie_url(params[:review][:movie_id])
+      end
+      format.js do
+
+      end
+    end
   end
 
 
